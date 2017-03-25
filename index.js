2e45c0702dad55c1278963d683b6ecce434d041a
Khai Phan <kphan95@bu.edu>
2017-03-25 15:20:39 -0400

    Fixed PORT Issues for Heroku Deployment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
 var path = require('path')
-var port = 5000
+
+app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static('public'))
 app.get('/', function (request, response) {
-  response.sendFile((path.join(__dirname + '/views/index.html')))
+  response.sendFile((path.join(__dirname, '/views/index.html')))
 })
 
-app.listen(port, function () {
-  console.log('Listening on port', port)
+app.listen(app.get('port'), function () {
+  console.log('Listening on port', app.get('port'))
 })
