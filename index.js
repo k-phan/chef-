99cbb15b1f06e24770d329fe3abd42bc719bf5b6
Khai Phan <kphan95@bu.edu>
2017-03-25 22:47:17 -0400

    Chefu App REST API

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var favicon = require('serve-favicon')
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static('public'))
 app.get('/', function (request, response) {
@@ -12,7 +15,14 @@ app.get('/', function (request, response) {
 })
 
 app.get('/chefu', function (request, response) {
-  response.send('THIS IS WHAT CHEFU WOULD BE')
+  response.sendFile((path.join(__dirname, '/views/chefu.html')))
+})
+
+// request.body.item1 && request.body.item2 have the two wanted items
+app.post('/ask_chefu', function (request, response) {
+  // do stuff & then send json or whatever
+  console.log(request.body)
+  response.redirect('/chefu')
 })
 
 app.listen(app.get('port'), function () {
